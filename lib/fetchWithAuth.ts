db707b552da3747855a25c87912c52b503b04dc4
AdriaOrozco <adria.orozco@vee.travel>
2025-07-15 12:38:21 +0200

    update Next cookie on prod

--- lib/fetchWithAuth.ts
+++ lib/fetchWithAuth.ts
@@ -2,12 +2,17 @@ import { cookies } from "next/headers";
 
 export async function fetchWithAuth(url: string, options: RequestInit = {}) {
   const cookieStore = await cookies();
-  const cookie = cookieStore.get("next-auth.session-token");
+  const token =
+    cookieStore.get("next-auth.session-token") ??
+    cookieStore.get("__Secure-next-auth.session-token");
+
+  if (!token) {
+    throw new Error("No session token found");
+  }
 
   const headers = new Headers(options.headers);
-  if (cookie) {
-    headers.set("Cookie", `next-auth.session-token=${cookie.value}`);
-  }
+  headers.set("Cookie", `${token.name}=${token.value}`);
+  console.log(`${process.env.NEXT_PUBLIC_APP_URL}/${url}`);
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/${url}`, {
     ...options,
